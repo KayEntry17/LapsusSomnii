bcbe0b0d68e758e790922f3b6c911eb324fec2e6
Kay <150719310+KayEntry17@users.noreply.github.com>
2025-12-07 20:10:21 +0000

    imported models, worked on shaders 4 one off the stages and planned out player

--- TheCrow/tests/crow_1ow.gd
+++ TheCrow/tests/crow_1ow.gd
@@ -19,3 +19,38 @@ extends Node3D
 #(if clunky its possible to just loose speed in an animation and destroy the obstacle)
 #in overworld the crow gets sent back a large distance and stops
 # barreling towards the ground plays a simple animation while transitioning the camera to an fps view to admire the sights and get dialogue
+
+@export_group("Speed")
+@export var speedperflap: float
+@export var startspeed: float
+@export var knockbackspeed: float
+@export var fullleanspeed: float
+@export var brakespeed: float
+@export var barrelrollspeed: float
+@export var maxbasespeed: float
+@export_group("Acceleration")
+@export var normaldesc: float
+@export var fullleandesceleration: float
+@export var brakedesc: float
+@export var dmgdesc: float
+@export_group("Timers")
+@export var spinouttime : float
+@export var flaptime: float
+@export var barrelrolltime: float
+@export var flapcooldown: float
+var spinouttimer : float=0
+var flaptimer: float=0
+var barrelrolltimer: float=0
+var flapcooldownr: float=0
+@export_group("Flags")
+@export var canflap: bool
+@export var isbrolling: bool
+@export var cooldown: bool
+@export_group("ControlMod")
+@export var Lean: Vector4=Vector4(1,1,1,1)
+@export var brake: Vector4=Vector4(1,1,1,1)
+@export var fullbrake:Vector2=Vector2(1,1)
+@export var broll:Vector2=Vector2(1,1)
+@export var damaged: Vector2=Vector2(1,1)
+func _process(delta: float) -> void:
+	pass
